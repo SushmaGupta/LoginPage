095743f69f8ae2543e70444c1bd60951f4e21ba3
SushmaGupta <35599552+SushmaGupta@users.noreply.github.com>
2018-04-04 14:42:12 +0100

    Create app.js

---
+++ scripts/app.js
@@ -0,0 +1,41 @@
+angular.module('Authentication', []);
+angular.module('Home', []);
+
+angular.module('AngularJsLoginLogout', [
+    'Authentication',
+    'Home',
+    'ngRoute',
+    'ngCookies'
+])
+ 
+.config(['$routeProvider', function ($routeProvider) {
+
+    $routeProvider
+        .when('/login', {
+            controller: 'LoginController',
+            templateUrl: 'modules/authentication/login.html',
+            hideMenus: true
+        })
+ 
+        .when('/', {
+            controller: 'HomeController',
+            templateUrl: 'modules/home/home.html'
+        })
+ 
+        .otherwise({ redirectTo: '/login' });
+}])
+ 
+.run(['$rootScope', '$location', '$cookieStore', '$http',
+    function ($rootScope, $location, $cookieStore, $http) {
+       
+        $rootScope.globals = $cookieStore.get('globals') || {};
+        if ($rootScope.globals.currentUser) {
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; 
+        }
+ 
+        $rootScope.$on('$locationChangeStart', function (event, next, current) {
+            if ($location.path() !== '/login' && !$rootScope.globals.currentUser) {
+                $location.path('/login');
+            }
+        });
+    }]);
